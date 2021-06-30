089a4ea3923a97e02e247ef806e2d559ac0e5500
Malik Umair Khalid <fighteromega@gmail.com>
2021-07-01 02:31:52 +0500

    details

---
+++ app.js
@@ -0,0 +1,68 @@
+var iphone = {
+    Brand1: {
+        name: "iphone 8",
+        Internal:"64GB 3GB RAM, 128GB 3GB RAM, 256GB 3GB RAM",
+        Weight:	"202g (7.13 oz)",
+        Build:"Glass front (Gorilla Glass), glass back (Gorilla Glass), aluminum frame",
+        SIM: "Nano-SIM",
+        camera: "12 MP, f/1.8, 28mm (wide), PDAF, OIS"
+    },
+    Brand2: {
+        name: "iphone x",
+        Internal:"64GB 3GB RAM, 256GB 3GB RAM",
+        Weight:	"174 g (6.14 oz)",
+        Build:"Glass front (Gorilla Glass), glass back (Gorilla Glass), stainless steel frame",
+        SIM: "Nano-SIM",
+        camera: "12 MP, f/1.8, 28mm (wide), 1/3, 1.22µm, dual pixel PDAF, OIS"
+    },
+    Brand3:{
+        name: "iphone 11",
+        Internal:"64GB 4GB RAM, 128GB 4GB RAM, 256GB 4GB RAM",
+        Weight:	"194 g (6.84 oz)",
+        Build:"Glass front (Gorilla Glass), glass back (Gorilla Glass), aluminum frame (7000",
+        SIM: "Glass front (Gorilla Glass), glass back (Gorilla Glass), aluminum frame (7000",
+        camera: "12 MP, f/1.8, 26mm (wide), 1/2.55, 1.4µm, dual pixel PDAF, OIS"
+    },
+    Brand4:{
+        name: "iphone 12",
+        Internal:"64GB 4GB RAM, 128GB 4GB RAM, 256GB 4GB RAM",
+        Weight:	"164 g (5.78 oz)",
+        Build:"Glass front (Gorilla Glass), glass back (Gorilla Glass), aluminum frame",
+        SIM: "Single SIM (Nano-SIM and/or eSIM) or Dual SIM (Nano-SIM, dual stand-by) - for China",
+        camera: "12 MP, f/1.6, 26mm (wide), 1.4µm, dual pixel PDAF, OIS 12 MP, f/2.4, 120˚, 13mm (ultrawide), 1/3.6"
+    }
+}
+var samsung = {
+    Brand1: {
+        name: "Samsung Galaxy S8",
+        Internal:"microSDXC (uses shared SIM slot) - dual SIM model only 64GB 4GB RAM",
+        Weight:	"155 g (5.47 oz)",
+        Build:"Glass front (Gorilla Glass 5), glass back (Gorilla Glass 5), aluminum frame",
+        SIM: "Single SIM (Nano-SIM) or Hybrid Dual SIM (Nano-SIM, dual stand-by) IP68 dust/water resistant (up to 1.5m for 30 mins)",
+        camera: "12 MP, f/1.7, 26mm (wide), 1/2.55, 1.4µm, dual pixel PDAF, OIS"
+    },
+    Brand2: {
+        name: "Samsung Galaxy S9",
+        Internal:"64GB 4GB RAM, 128GB 4GB RAM, 256GB 4GB RAM",
+        Weight:	"163 g (5.75 oz)",
+        Build:"Glass front (Gorilla Glass 5), glass back (Gorilla Glass 5), aluminum frame",
+        SIM: "Nano-SIM",
+        camera: "Single SIM (Nano-SIM) or Hybrid Dual SIM (Nano-SIM, dual stand-by) IP68 dust/water resistant (up to 1.5m for 30 mins)"
+    },
+    Brand3:{
+        name: "Samsung Galaxy S10",
+        Internal:"128GB 8GB RAM, 512GB 8GB RAM",
+        Weight:	"157 g (5.54 oz)",
+        Build:"Glass front (Gorilla Glass 6), glass back (Gorilla Glass 5), aluminum frame",
+        SIM: "Single SIM (Nano-SIM) or Hybrid Dual SIM (Nano-SIM, dual stand-by) IP68 dust/water resistant (up to 1.5m for 30 mins)",
+        camera: "12 MP, f/1.5-2.4, 26mm (wide), 1/2.55, 1.4µm, Dual Pixel PDAF, OIS  12 MP, f/2.4, 52mm (telephoto), 1/3.6, 1.0µm, AF, OIS, 2x optical zoom"
+    },
+    Brand4:{
+        name: "iphone 12",
+        Internal:"64GB 4GB RAM, 128GB 4GB RAM, 256GB 4GB RAM",
+        Weight:	"175 g / 198 g (ceramic) (6.17 oz)",
+        Build:"Glass front (Gorilla Glass 6), glass back (Gorilla Glass 5), aluminum frame Glass front (Gorilla Glass 6), ceramic back, ceramic frame",
+        SIM: "Single SIM (Nano-SIM) or Hybrid Dual SIM (Nano-SIM, dual stand-by) IP68 dust/water resistant (up to 1.5m for 30 mins)",
+        camera: "12 MP, f/1.5-2.4, 26mm (wide), 1/2.55, 1.4µm, Dual Pixel PDAF, OIS  12 MP, f/2.4, 52mm (telephoto), 1/3.6, 1.0µm, AF, OIS, 2x optical zoom"
+    }
+}
